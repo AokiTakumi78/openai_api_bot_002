e92ab15cdec1d01b2de862b3c11cd7a3b35b9160
AokiTakumi78 <135187106+AokiTakumi78@users.noreply.github.com>
2023-06-21 16:20:53 +0900

    Update app.py

--- app.py
+++ app.py
@@ -35,8 +35,8 @@ def communicate():
 
 
 # ユーザーインターフェイスの構築
-st.title("My AI Assistant")
-st.write("ChatGPT APIを使ったチャットボットです。")
+st.title("Finance News Assistant")
+st.write("ChatGPT APIを使ったFinance News ドラフト紹介ボットです。")
 
 user_input = st.text_input("メッセージを入力してください。", key="user_input", on_change=communicate)
 
