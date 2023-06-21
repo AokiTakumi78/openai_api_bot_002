c707bab1bd9559361e4b1b6898e443b195ea261b
AokiTakumi78 <135187106+AokiTakumi78@users.noreply.github.com>
2023-06-21 16:28:48 +0900

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,9 @@ openai.api_version = "2023-03-15-preview"
 # st.session_stateを使いメッセージのやりとりを保存
 if "messages" not in st.session_state:
     st.session_state["messages"] = [
-        {"role": "system", "content": "あなたは優秀なアシスタントAIです。"}
+        {"role":"system","content":"あなたは、社内向けのファイナンスニュース紹介文書のドラフト生成をするAIアシスタントです。"},
+        {"role":"user","content":"最新AIニュースの紹介文を作ってください。"},
+        {"role":"assistant","content":"おはようございます\nFinTech Product Strategy Gr〇●です\n\n夢の、ドキュメント自動化が近づいています。\n\nみずほ、システム開発・保守を生成AIで改善　富士通と実証実験\nhttps://www.itmedia.co.jp/news/articles/2306/19/news149.html\n\nーーーー\n富士通は6月19日、みずほフィナンシャルグループのシステム開発・保守作業を改善する実証実験を始めたと発表した。生成AIを活用し、システム設計書の記載漏れや書き間違いを自動検出 するという。2024年3月31日まで2社共同で実施する。\n\n　設計書の記載漏れを防ぐだけでなく、修正した書類を基に、システムテストの仕様書をAIで自動生成する技術の開発 も目指す。システムの開発・保守そのものをAIに任せることが可能かの検証も進めるという。\nーーーー"}
         ]
 
 # チャットボットとやりとりする関数
